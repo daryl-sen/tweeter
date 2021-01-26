3ee63f538814dcec702497670012ef1ed3c26bcf
daryl <tsdaryl@gmail.com>
2021-01-26 14:09:46 -0800

    fixed composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,27 @@
+// $(document).ready(function() {
+//   $('#tweet-text').on('keyup', function() {
+//     // console.log(this);
+//     let newVal = 140 - this.value.length;
+//     $('.counter').val(newVal);
+//     if (newVal < 0) {
+//       $('.counter').addClass('red');
+//     } else {
+//       $('.counter').removeClass('red');
+//     }
+//   });
+// });
+
 $(document).ready(function() {
   $('#tweet-text').on('keyup', function() {
-    // console.log(this);
-    let newVal = 140 - this.value.length;
-    $('.counter').val(newVal);
+    let newVal = 140 - $(this).val().length;
+    // why???
+    const counter = $(this).parent().children('#tweetButtonAndCounter').children('output')
+    counter.val(newVal);
     if (newVal < 0) {
-      $('.counter').addClass('red');
+      counter.addClass('red');
     } else {
-      $('.counter').removeClass('red');
+      counter.removeClass('red');
     }
   });
 });
+
