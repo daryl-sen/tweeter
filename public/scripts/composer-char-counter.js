281c11c197a1cb7d1076688da263e40ceff7b011
daryl <tsdaryl@gmail.com>
2021-01-26 16:42:25 -0800

    implemented mentor suggestions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,23 @@
-// $(document).ready(function() {
-//   $('#tweet-text').on('keyup', function() {
-//     // console.log(this);
-//     let newVal = 140 - this.value.length;
-//     $('.counter').val(newVal);
-//     if (newVal < 0) {
-//       $('.counter').addClass('red');
-//     } else {
-//       $('.counter').removeClass('red');
-//     }
-//   });
-// });
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function() {
+    // console.log(this);
+    let newVal = 140 - this.value.length;
+    $('.counter').val(newVal);
+    if (newVal < 0) {
+      $('.counter').addClass('red');
+    } else {
+      $('.counter').removeClass('red');
+    }
+  });
+});
+
+// no global search - slower, performance penalty
+// if .counter is used in multiple locations
 
 $(document).ready(function() {
   $('#tweet-text').on('keyup', function() {
-    let newVal = 140 - $(this).val().length;
-    // why???
+    const newVal = 140 - $(this).val().length; // default to const not let
+    // for (const item of array) use const
     const counter = $(this).parent().children('#tweetButtonAndCounter').children('output')
     counter.val(newVal);
     if (newVal < 0) {
