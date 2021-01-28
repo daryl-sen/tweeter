6b1f47783df9dddbb7a0649327dcaaae71ddb7e8
daryl <tsdaryl@gmail.com>
2021-01-27 17:28:14 -0800

    load tweets function working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-console.log('client loaded');
-
-
 
 const createTweetElement = function(tweetData) {
   return `<article>
@@ -34,39 +31,48 @@ const createTweetElement = function(tweetData) {
 
 const renderTweets = function(tweetsArray, tweetsContainerNode, processTweet) {
   for (const tweet of tweetsArray) {
-    tweetsContainerNode.append(processTweet(tweet));
+    $('#tweets').append(processTweet(tweet));
   }
+};
+
+const loadTweets = function(callback) {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET'
+  })
+    .then((result) => {
+      callback(result, $('#tweets'), createTweetElement);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 
-$( document ).ready(function() {
-  renderTweets(data, $('#tweets'), createTweetElement);
+
+
+$(document).ready(function() {
+  // renderTweets(data, $('#tweets'), createTweetElement);
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const qstring = $(this).serialize();
+    $.ajax({
+      url: '/tweets/',
+      method: 'GET'
+    })
+      .then((result) => {
+        console.log(typeof(result));
+        renderTweets(result, $('#tweets'), createTweetElement);
+      })
+      .catch((err) => {
+        console.log('something bad happened');
+      })
+  });
+
+  loadTweets(renderTweets);
 });
